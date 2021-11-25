4984a61ec114a9a646cf4e59e7ff110af2ba1bf1
nikistefanov <nikolay.n.stefanov@gmail.com>
2021-11-25 16:13:52 +0200

    Initial commit

---
+++ tu-erasmus-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4a596aef8423869341972330c8e88b6c'),
+    },
+  },
+});
---
+++ tu-erasmus-server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4c683a2a-652a-4a35-9a61-95c1c5080a97'
+};
